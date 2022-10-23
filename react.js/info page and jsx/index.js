d431b36bc8ecbc6b3731108e8594ceef3d3a8e0c
Kaden Carr <kccarr6022@eagle.fgcu.edu>
2022-10-23 16:16:11 -0400

    react tutorials

---
+++ react.js/info page and jsx/index.js
@@ -0,0 +1,10 @@
+//JSX
+
+const page = (
+    <div>
+        <h1 className="header">This is JSX</h1>
+        <p>This is a paragraph</p>
+    </div>
+)
+
+ReactDOM.render(page, document.getElementById("root"));
---
+++ react.js/sub-tutorials/imperative programming/vanillajsreact.js
@@ -0,0 +1,5 @@
+const h1 = document.createElement("h1");
+h1.textContent = "this is an imperative way to program";
+h1.className = "header";
+console.log(h1)
+document.getElementById("root").append(h1);
